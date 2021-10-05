00b794c2104c7b746fa5740b9c9949ebedb525e4
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2021-10-05 23:20:12 +0530

    Updated with comparison between various parameters with different setups

--- PK041021-G01/subplotter.py
+++ PK041021-G01/subplotter.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import re
 
 flag = 1
+subplot_num = 231
 
 def read_file(extension):
 	fp = open(extension + "data.txt", 'r')
@@ -23,7 +24,7 @@ def read_file(extension):
 		extr = re.findall("[0-9.]+", line)
 		try:
 			if(len(extr) == 6):
-				print(extr)
+				#print(extr)
 				if(float(extr[1]) != target_flags):
 					timestamps = np.append(timestamps, float(extr[0]))
 					target_flags = float(extr[1])
@@ -35,29 +36,55 @@ def read_file(extension):
 				k = np.append(k, float(extr[5]))
 		except:
 			continue
+	print("\aDone")
 	return t, timestamps, r_f, SFM, DP, SDP, k
 
 def plot(x, y, timestamps, xlabel, ylabel, title, extension):
-	global flag
-	plt.figure(flag)
+	global flag, subplot_num
+	plt.figure(title)
+	plt.subplot(subplot_num)
 	plt.plot(x, y, "k.")
 	if(len(timestamps) > 0):
 		for t in timestamps:
 			plt.axvline(t, color ='r')
 	plt.xlabel(xlabel)
 	plt.ylabel(ylabel)
-	plt.title(title)
-	plt.savefig(extension + title + ".png")
+	if(subplot_num == 231):
+		name = "SFM"
+	elif(subplot_num == 232):
+		name = "SFM + 30 cm tube"
+	elif(subplot_num == 233):
+		name = "SFM + 30 cm tube + #00"
+	elif(subplot_num == 234):
+		name = "SFM + 30 cm tube + #0"
+	elif(subplot_num == 235):
+		name = "SFM + 30 cm tube + #1"
+	elif(subplot_num == 236):
+		name = "SFM + 30 cm tube + #2"
+	plt.title(name)
 	flag += 1
+	if(subplot_num == 236):
+		plot_backend = plt.get_backend()
+		mng = plt.get_current_fig_manager()
+		if plot_backend == 'TkAgg':
+			mng.resize(*mng.window.maxsize())
+		elif plot_backend == 'wxAgg':
+			mng.frame.Maximize(True)
+		elif plot_backend == 'Qt4Agg':
+			mng.window.showMaximized()
+		fig = plt.gcf()
+		fig.set_size_inches(32, 18)
+		fig.subplots_adjust(wspace=0.455, 
+						hspace=0.455)
+		fig.show()
+		fig.savefig("D:\\KYRON\\Code\\DATA\\Nasal Cannula Data\\PK041021-G01\\REPORT\\" + title + ".png", dpi = 500)
 	return 0
 
-if __name__ == "__main__":
-	extension = "D:\\KYRON\\Code\\DATA\\Nasal Cannula Data\\PK041021-G01\\Red\\"
-	print("EXTENSION: " + extension)
-	ch = input("")
+def manager(extension):
+	global flag, subplot_num
+	print(extension)
 	t, timestamps, r_f, SFM, DP, SDP, k = read_file(extension)
-	print(t)
-	print(SFM)
+	
 	plot(t, SFM, timestamps, "Time in sec", "Flow through SFM", "SFM vs Time", extension)
 	plot(t, DP, timestamps, "Time in sec", "Differential Pressure", "Differential Pressure vs Time", extension)
 	plot(t, SDP, timestamps, "Time in sec", "Prediction of flow", "Variation in prediction of flow with time and targets", extension)
@@ -65,4 +92,23 @@ if __name__ == "__main__":
 	plot(r_f, SFM, [], "Target", "Flow through SFM", "SFM vs Target", extension)
 	plot(r_f, SDP, [], "Target", "Prediction of flow", "Variation in prediction of flow with targets", extension)
 	plot(r_f, k, [], "Target", "PWM (0 - 255)", "Variation in PWM with different targets", extension)
-	plt.show()
+	flag = 1
+	subplot_num += 1
+	return 0
+
+if __name__ == "__main__":
+	extension = "D:\\KYRON\\Code\\DATA\\Nasal Cannula Data\\PK041021-G01\\"
+	'''
+	1. extract data from a particular text file
+	2. plot in figures consisting of subplots 
+	3. update the subplot number (for the next data)
+	4. reset the global figure flag back to 1.
+	5. change the directory
+	6. Go to step 1
+	'''
+	manager(extension + "Without tube\\")
+	manager(extension + "With tube\\")
+	manager(extension + "00\\")
+	manager(extension + "0\\")
+	manager(extension + "1\\")
+	manager(extension + "2\\")
